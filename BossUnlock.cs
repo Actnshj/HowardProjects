c393bbaf974e62bffce441076909ae3fb8a7e45c
Actnshj <56264272+Actnshj@users.noreply.github.com>
2019-11-27 15:03:50 -0500

    TextAdv(Boss Unlock) 2007 First C# Console Project
    Boss Unlock Logic for the Text Adventure

---
+++ BossUnlock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BossUnlocking
+{
+    class BossUnlock
+    {
+        public static bool Eyesman = false;
+        public static bool PidgeonHead = false;
+        public static bool Cobaltp = false;
+        public static bool GiantS = false;
+        public static bool Titan = false;
+        public static bool MudMonster = false;
+        public static bool MadScientist = false;
+        public static bool AppMag = false;
+        public static bool VoidS = false;
+        public static bool GiantHand = false;
+        public static bool StoneOgre = false;
+        public static bool ColosslEye = false;
+        public static bool QueenP = false;
+        public static bool ElfLord = false;
+        public static bool MasterWood= false;
+        public static bool SonicW = false;
+        public static bool GoldenSam = false;
+        public static bool SenseiGouk = false;
+        public static bool CyberneticCow = false;
+        public static bool Kraken = false;
+        public static bool Abyss1 = false;
+        public static bool GiantSerp = false;
+        public static bool TwilightSw = false;
+        public static bool Abyss2 = false;
+        public static bool other = false;
+
+    }
+}
